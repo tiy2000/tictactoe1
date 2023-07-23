bc234d7e0668f055ac01c059a3c9080532ed0ca8
Igor Timofeev <tiy2000@mail.ru>
2023-07-23 16:22:26 +0700

    ADD: Game logic

---
+++ src/main/java/com/github/tiy2000/tictactoe/game/GameLogic.java
@@ -0,0 +1,45 @@
+package com.github.tiy2000.tictactoe.game;
+
+import com.github.tiy2000.tictactoe.game.board.Board;
+import com.github.tiy2000.tictactoe.game.player.Player;
+import com.github.tiy2000.tictactoe.ui.Console;
+
+public class GameLogic {
+
+    private final Console console;
+    private final Board board;
+    private final Player player1;
+    private final Player player2;
+
+    public GameLogic(Console console, Board board, Player player1, Player player2) {
+        this.console = console;
+        this.board = board;
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+
+    public void play() {
+        console.println("Game is started");
+        board.print();
+        while (true) {
+            if (playerMove(console, board, player1)) break;
+            if (playerMove(console, board, player2)) break;
+        }
+        console.println("Game is finished");
+    }
+
+    private boolean playerMove(Console console, Board board, Player player) {
+        console.print(player.getDescription() + " move. Enter row and column, divided by space: ");
+        player.move(board);
+        board.print();
+        if (board.isWinner(player.getSymbol())) {
+            console.println(player.getDescription() + " won!");
+            return true;
+        }
+        if (board.isFilled()) {
+            console.println("Dead heat!");
+            return true;
+        }
+        return false;
+    }
+}
